bece75b14bc8e05dfbf0649d705326af75b847d2
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-12-28 14:53:57 +0900

    Add upload.rb

---
+++ script/upload.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk-s3'
+
+ACCESS_KEY_ID = ENV['ACCESS_KEY_ID']
+SECRET_ACCESS_KEY = ENV['SECRET_ACCESS_KEY']
+BUCKET_NAME = ENV['BUCKET_NAME']
+
+SRC_DIR = '../src/'
+
+s3 = Aws::S3::Resource.new(region: 'ap-northeast-1',
+                           access_key_id: ACCESS_KEY_ID,
+                           secret_access_key: SECRET_ACCESS_KEY)
+bucket = s3.bucket(BUCKET_NAME)
+
+Dir.glob(File.join(SRC_DIR, '**', '*.html')).each do |path|
+  key = path.sub(SRC_DIR, '')
+
+  obj = s3.bucket(BUCKET_NAME).object(key)
+  p path
+  obj.upload_file(path)
+end
